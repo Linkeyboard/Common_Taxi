80a0361358156da48f27ad5e3c8477a33d260e53
lin <lk@acm.hitwh.edu.cn>
2017-06-08 16:53:41 +0800

    You should add Blueprint

--- Common_D/hello.py
+++ Common_D/hello.py
@@ -54,5 +54,5 @@ def Mapserch():
 
 
 if __name__ == '__main__':
-    app.run(debug = True, host ='0.0.0.0', port=5000) 
+    app.run(debug = True, host ='0.0.0.0', port=8008) 
 
---
+++ Common_D/test2.py
@@ -0,0 +1,14 @@
+from flask import Flask, Blueprint
+
+app=Flask(__name__,static_url_path='/lk/static')
+
+
+
+webapp = Blueprint('webapp', __name__,url_prefix='/lk')
+
+@webapp.route('/a')
+def Index():
+    return "hello world"
+
+app.register_blueprint(webapp)
+app.run(host='0.0.0.0',port=8008)
