6773fc2d19a7dd65a69859da33fcca6d789a16d6
lin <lk@acm.hitwh.edu.cn>
2017-06-14 10:41:09 +0800

    Test sendmessages

--- Common_D/send_templateimesages.py
+++ Common_D/send_templateimesages.py
@@ -2,33 +2,42 @@ import requests
 import json
 from get_accesstoken import access_token 
 
-data = {
-           "touser":"oqK-bxCCAxRaEHslFrJ7_UGQ8JNM",
-           "template_id":"66-Ti5J6XyTzMXbVHPH-cr2NZ_xeNIHNK4FnmCJTl9Q",
-           "url":"http://weixin.qq.com/download",  
-           "data":{
-                   "first": {
-                       "value":"恭喜你购买成功！",
-                       "color":"#173177"
-                   },
-                   "keyword1":{
-                       "value":"巧克力",
-                       "color":"#173177"
-                   },
-                   "keyword2": {
-                       "value":"39.8元",
-                       "color":"#173177"
-                   },
-                   "keyword3": {
-                       "value":"2014年9月22日",
-                       "color":"#173177"
-                   },
-                   "remark":{
-                       "value":"欢迎再次购买！",
-                       "color":"#173177"
-                   }
-           }
-       }
+senddata = {
+    "touser":'oqK-bxCCAxRaEHslFrJ7_UGQ8JNM',
+    "template_id":"9P24eASJx9m5yNSXbA6wo4qf6HOaWhJBt35pN1XZh1M",
+    "url":"https://www.baidu.com",
+    "data":{
+        "first": {
+            "value":"提示：加入活动成功！",
+            "color":"#173177"
+            },
+        "keynote1":{
+            "value":"asdf",
+            "color":"#173177"
+            },
+        "keynote2": {
+            "value":"aaa",
+            "color":"#173177"
+            },
+        "keynote3": {
+            "value":"aaa",
+            "color":"#173177"
+            },
+        "keynote4": {
+            "value":"aaa",
+            "color":"#173177"
+            },
+        "keynote5": {
+            "value":"aaa",
+            "color":"#173177"
+            },
+        "remark":{
+            "value":"请按时到达集合地！",
+            "color":"#173177"
+            }
+    }
+}
+message = requests.post('https://api.weixin.qq.com/cgi-bin/message/template/send?access_token='+access_token,data = json.dumps(senddata))
+print(message)
 
 
-message = requests.post("https://api.weixin.qq.com/cgi-bin/message/template/send?access_token="+access_token,data = json.dumps(data))
